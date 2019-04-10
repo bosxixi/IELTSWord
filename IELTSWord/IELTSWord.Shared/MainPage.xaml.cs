1f1189217ccc1ecc035aaf66fc7a57a986101321
bosxixi <bosxixi@outlook.com>
2019-04-10 23:30:20 +0800

    uri

--- IELTSWord/IELTSWord.Shared/MainPage.xaml.cs
+++ IELTSWord/IELTSWord.Shared/MainPage.xaml.cs
@@ -25,46 +25,13 @@ using System.Net.Http;
 using System.Text;
 using Windows.UI.Popups;
 using Newtonsoft.Json;
-using System.Xml.Linq;
 using System.Diagnostics;
-using Windows.Media.Playback;
 using Windows.Media.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace IELTSWord
 {
-    public class Authentication
-    {
-        private string subscriptionKey;
-        private string tokenFetchUri;
-
-        public Authentication(string tokenFetchUri, string subscriptionKey)
-        {
-            if (string.IsNullOrWhiteSpace(tokenFetchUri))
-            {
-                throw new ArgumentNullException(nameof(tokenFetchUri));
-            }
-            if (string.IsNullOrWhiteSpace(subscriptionKey))
-            {
-                throw new ArgumentNullException(nameof(subscriptionKey));
-            }
-            this.tokenFetchUri = tokenFetchUri;
-            this.subscriptionKey = subscriptionKey;
-        }
-
-        public async Task<string> FetchTokenAsync()
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", this.subscriptionKey);
-                UriBuilder uriBuilder = new UriBuilder(this.tokenFetchUri);
-
-                HttpResponseMessage result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null).ConfigureAwait(false);
-                return await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-            }
-        }
-    }
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -94,29 +61,23 @@ namespace IELTSWord
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Level)));
             }
         }
-        MediaPlayer _media;
-        byte[] _audio;
+        Windows.Media.Playback.MediaPlayer _media;
         async void Speaker_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 if (this.CurrentWord?.Name != null)
                 {
-                    if (_audio == null)
-                    {
-                        _audio = await GetAudioAsync(this.CurrentWord.Name);
-                    }
                     if (_media == null)
                     {
-                        _media = new MediaPlayer();
+                        _media = new Windows.Media.Playback.MediaPlayer();
                     }
-                    _media.Source = MediaSource.CreateFromStream(_audio.ToMemoryStream().AsRandomAccessStream(), "audio/wav");
+                    _media.Source = MediaSource.CreateFromUri(new Uri($"https://searchscorpio.azurewebsites.net/api/google/ToSpeech?word={Uri.EscapeDataString(this.CurrentWord.Name)}"));
                     _media.AutoPlay = true;
                 }
             }
             catch (Exception ex)
             {
-                _accessToken = null;
                 MessageDialog messageDialog = new MessageDialog(ex.Message, "Error");
                 await messageDialog.ShowAsync();
             }
@@ -433,89 +394,6 @@ namespace IELTSWord
             return words.DistinctBy(c => c.Name).ToList();
         }
 
-        static string _accessToken;
-        static async Task<byte[]> GetAudioAsync(string text)
-        {
-            // Prompts the user to input text for TTS conversion
-            Debug.Write("What would you like to convert to speech? ");
-
-            // Gets an access token
-
-            Debug.WriteLine("Attempting token exchange. Please wait...\n");
-
-            // Add your subscription key here
-            // If your resource isn't in WEST US, change the endpoint
-            Authentication auth = new Authentication("https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken", SPEECH_KEY);
-            try
-            {
-                if (_accessToken == null)
-                {
-                    _accessToken = await auth.FetchTokenAsync().ConfigureAwait(false);
-                    Debug.WriteLine("Successfully obtained an access token. \n");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Failed to obtain an access token.");
-                Debug.WriteLine(ex.ToString());
-                Debug.WriteLine(ex.Message);
-                return null;
-            }
-
-            string host = "https://eastasia.tts.speech.microsoft.com/cognitiveservices/v1";
-
-            // Create SSML document.
-            XDocument body = new XDocument(
-                    new XElement("speak",
-                        new XAttribute("version", "1.0"),
-                        new XAttribute(XNamespace.Xml + "lang", "en-US"),
-                        new XElement("voice",
-                            new XAttribute(XNamespace.Xml + "lang", "en-US"),
-                            new XAttribute(XNamespace.Xml + "gender", "Female"),
-                            new XAttribute("name", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)"),
-                            text)));
-
-            using (HttpClient client = new HttpClient())
-            {
-                using (HttpRequestMessage request = new HttpRequestMessage())
-                {
-                    // Set the HTTP method
-                    request.Method = HttpMethod.Post;
-                    // Construct the URI
-                    request.RequestUri = new Uri(host);
-                    // Set the content type header
-                    request.Content = new StringContent(body.ToString(), Encoding.UTF8, "application/ssml+xml");
-                    // Set additional header, such as Authorization and User-Agent
-                    request.Headers.Add("Authorization", "Bearer " + _accessToken);
-                    request.Headers.Add("Connection", "Keep-Alive");
-                    // Update your resource name
-                    request.Headers.Add("User-Agent", "YOUR_RESOURCE_NAME");
-                    // Audio output format. See API reference for full list.
-                    request.Headers.Add("X-Microsoft-OutputFormat", "riff-24khz-16bit-mono-pcm");
-                    // Create a request
-                    Debug.WriteLine("Calling the TTS service. Please wait... \n");
-                    using (HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false))
-                    {
-                        response.EnsureSuccessStatusCode();
-                        return await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
-                        // Asynchronously read the response
-                        //using (Stream dataStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
-                        //{
-                        //    Debug.WriteLine("Your speech file is being written to file...");
-                        //    MemoryStream ms = new MemoryStream();
-                        //    await dataStream.CopyToAsync(ms).ConfigureAwait(false);
-                        //    //using (FileStream fileStream = new FileStream(@"sample.wav", FileMode.Create, FileAccess.Write, FileShare.Write))
-                        //    //{
-                        //    //    await dataStream.CopyToAsync(fileStream).ConfigureAwait(false);
-                        //    //    fileStream.Close();
-                        //    //}
-                        //    Debug.WriteLine("\nYour file is ready. Press any key to exit.");
-                        //    return ms;
-                        //}
-                    }
-                }
-            }
-        }
 
 
         List<Word> Words;
@@ -525,7 +403,7 @@ namespace IELTSWord
         public Word CurrentWord
         {
             get { return _word; }
-            set { _word = value; _audio = null; }
+            set { _word = value; }
         }
 
         Dictionary<string, string> Dics = new Dictionary<string, string>()
