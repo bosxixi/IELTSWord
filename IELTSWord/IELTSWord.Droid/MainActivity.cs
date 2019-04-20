e835d58aff043cb91ac4b85a8d66a6752c998db0
bosxixi <bosxixi@outlook.com>
2019-04-20 14:10:28 +0800

    res

--- IELTSWord/IELTSWord.Droid/MainActivity.cs
+++ IELTSWord/IELTSWord.Droid/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
 using Android.Views;
+using System.IO;
 
 namespace IELTSWord.Droid
 {
@@ -16,14 +17,22 @@ namespace IELTSWord.Droid
         )]
     public class MainActivity : Windows.UI.Xaml.ApplicationActivity
     {
+        public static MainActivity Instance;
         protected override void OnCreate(Bundle bundle)
         {
+            Instance = this;
             base.OnCreate(bundle);
 
             if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
             {
                 Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 45, 45, 48));
             }
+            //System.IO.Stream input = Assets.Open("my_asset.txt");
+        }
+
+        public Stream OpenAsset(string folder, string file)
+        {
+            return  Assets.Open($"{folder}/{file}");
         }
     }
 }
